1d472c89f2734638076dd255c21318eb011d6124
Banugoban <gobanbanu749@gmail.com>
2024-08-28 15:19:38 +0530

    Update Home.js h1

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -47,6 +47,7 @@ export default function Home() {
 
       <div>
         <h1 style={{textAlign:"center"}}>We are Digital marketing Company in Coimbatore</h1>
+  <h1 style={{textAlign:"center"}}>We are Digital marketing Company in Coimbatore</h1>
       </div>
     </div>
   );
